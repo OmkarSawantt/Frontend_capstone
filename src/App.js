99a9958f49b5de779a2d6667535882280455253f
OmkarSawantt <162661858+OmkarSawantt@users.noreply.github.com>
2024-06-22 11:43:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
- 
     <div>
-      <h1>Meta Frontend Developer Capstone Starter Files</h1>
+    <React.Fragment>
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
     </div>
   );
 }
